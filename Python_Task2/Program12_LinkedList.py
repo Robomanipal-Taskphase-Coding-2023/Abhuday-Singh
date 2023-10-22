81511dc359e7362134810ad952efae443574a099
Abhuday Singh <147344196+ZaGrayWolf@users.noreply.github.com>
2023-10-22 16:49:44 +0530

    Create Program12_LinkedList.py

---
+++ Python_Task2/Program12_LinkedList.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head is None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def display(self):
+        elements = []
+        current = self.head
+        while current:
+            elements.append(current.data)
+            current = current.next
+        return elements
