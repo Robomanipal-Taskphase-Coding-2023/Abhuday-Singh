637dbf9491c6dc596d22109581a2e7d0b6991f20
Abhuday Singh <147344196+ZaGrayWolf@users.noreply.github.com>
2023-10-25 15:52:28 +0530

    Update Program1.py

--- Python_Task2/Program1.py
+++ Python_Task2/Program1.py
@@ -1,5 +1,16 @@
-a=float(input("Enter num 1: "))
-b=float(input("Enter num 2: "))
-c=float(input("Enter num 3: "))
-maxnum=maxnum(a,b,c)
-print('The maximum number is: ',max)
+# Input three numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+# Compare the numbers to find the largest
+if num1 >= num2 and num1 >= num3:
+    largest = num1
+elif num2 >= num1 and num2 >= num3:
+    largest = num2
+else:
+    largest = num3
+
+# Print the result
+print("The largest number is:", largest)
+
