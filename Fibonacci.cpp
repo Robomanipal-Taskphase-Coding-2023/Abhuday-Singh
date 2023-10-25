f01cd2d87c1e7554de2871d2fd3b8109ac0f963c
Abhuday Singh <147344196+ZaGrayWolf@users.noreply.github.com>
2023-10-25 15:16:01 +0530

    Update Fibonacci.cpp

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -1,40 +1,19 @@
 #include <iostream>
 using namespace std;
 
-static constexpr int N = 40;
-int map[N];
-
-//approx. execution time: 0 seconds
 int fibonacci(int n) {
-
-	if (n <= 1) {
-		return n;
-	}
-
-	if (map[n - 1] != -1) {
-		return map[n - 1];
-	}
-
-	int fib = fibonacci(n - 1) + fibonacci(n - 2);
-	map[n - 1] = fib;
-	return fib;
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-void fibonacci(void) {
-
-	time_t start, end;
-	time(&start);
-
-	for (int i = 1; i < N; i++) {
-		map[i] = -1;
-	}
-
-	for (int i = 1; i <= N; i++) {
-		cout << fibonacci(i) << endl;
-	}
-
-	time(&end);
-	int exetime = int(end - start);
-
-	cout << "\nExecution time: " << exetime << " sec\n";
+int main() {
+    cout << "The Fibonacci series is" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << fibonacci(i) << endl;
+    }
+    return 0;
 }
+
+
