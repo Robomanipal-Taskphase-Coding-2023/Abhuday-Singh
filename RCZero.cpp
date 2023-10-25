7b51cbc4000ab0ad5aa4072eed67e4b89fc6d40e
Abhuday Singh <147344196+ZaGrayWolf@users.noreply.github.com>
2023-10-25 15:30:46 +0530

    Update RCZero.cpp

--- RCZero.cpp
+++ RCZero.cpp
@@ -1,42 +1,57 @@
 #include <iostream>
-#include "helpers.h"
 using namespace std;
 
-void rczero(void) {
+// Function to print a matrix
+void matprint(int** matrix, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << ' ';
+        }
+        cout << endl;
+    }
+}
 
-	int ra, ca;
-	cout << "Enter rows and columns in array\n";
-	cin >> ra >> ca;
+void rczero() {
+    int ra, ca;
+    cout << "Enter rows and columns in the array: ";
+    cin >> ra >> ca;
 
-	bool** tz = new bool* [ra];
-	for (int i = 0; i < ra; i++) {
-		tz[i] = new bool[2];
-		for (int j = 0; j < 2; j++) {
-			tz[i][j] = false;
-		}
-	}
+    bool** tz = new bool*[ra];
+    for (int i = 0; i < ra; i++) {
+        tz[i] = new bool[2];
+        tz[i][0] = false; // Initialize to false
+        tz[i][1] = false;
+    }
 
-	cout << "Enter matrix row-wise\n";
-	int** a = new int* [ra];
-	for (int i = 0; i < ra; i++) {
-		a[i] = new int[ca];
-		for (int j = 0; j < ca; j++) {
-			cin >> a[i][j];
-			if (a[i][j] == 0) {
-				tz[i][0] = true;
-				tz[j][1] = true;
-			}
-		}
-	}
+    int** a = new int*[ra];
+    for (int i = 0; i < ra; i++) {
+        a[i] = new int[ca];
+    }
 
-	for (int i = 0; i < ra; i++) {
-		for (int j = 0; j < ca; j++) {
-			if (tz[j][1] || tz[i][0]) {
-				a[i][j] = 0;
-			}
-		}
-	}
+    cout << "Enter matrix row-wise:\n";
+    for (int i = 0; i < ra; i++) {
+        for (int j = 0; j < ca; j++) {
+            cin >> a[i][j];
+            if (a[i][j] == 0) {
+                tz[i][0] = true;
+                tz[j][1] = true;
+            }
+        }
+    }
 
-	cout << "Adjusted matrix is:\n";
-	intmatprint(a, ra, ca);
+    for (int i = 0; i < ra; i++) {
+        for (int j = 0; j < ca; j++) {
+            if (tz[i][0] || tz[j][1]) {
+                a[i][j] = 0;
+            }
+        }
+    }
+
+    cout << "Adjusted matrix is:\n";
+    matprint(a, ra, ca);
+}
+
+int main() {
+    rczero();
+    return 0;
 }
