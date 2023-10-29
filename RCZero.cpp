9a8ddadb1b83ad158e24c142ea79cefdba0a5668
Abhuday Singh <147344196+ZaGrayWolf@users.noreply.github.com>
2023-10-29 20:11:36 +0530

    Update RCZero.cpp

--- RCZero.cpp
+++ RCZero.cpp
@@ -1,57 +1,42 @@
-#include <iostream>
-using namespace std;
-
-// Function to print a matrix
-void matprint(int** matrix, int rows, int cols) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            cout << matrix[i][j] << ' ';
-        }
-        cout << endl;
-    }
-}
-
-void rczero() {
-    int ra, ca;
-    cout << "Enter rows and columns in the array: ";
-    cin >> ra >> ca;
-
-    bool** tz = new bool*[ra];
-    for (int i = 0; i < ra; i++) {
-        tz[i] = new bool[2];
-        tz[i][0] = false; // Initialize to false
-        tz[i][1] = false;
-    }
-
-    int** a = new int*[ra];
-    for (int i = 0; i < ra; i++) {
-        a[i] = new int[ca];
-    }
-
-    cout << "Enter matrix row-wise:\n";
-    for (int i = 0; i < ra; i++) {
-        for (int j = 0; j < ca; j++) {
+#include <iostream> 
+#include <vector>
+using namespace std; 
+int main()
+{
+    int r,c,i,j,k;
+    cout << "Enter rows and columns in the array: "; 
+    cin >> r >> c; 
+    int a[r][c];
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
             cin >> a[i][j];
-            if (a[i][j] == 0) {
-                tz[i][0] = true;
-                tz[j][1] = true;
+        }
+    }
+for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            if(a[i][j]==0)
+            {
+                for(k=0;k<r;k++)
+                a[i][k]=0;
             }
         }
     }
 
-    for (int i = 0; i < ra; i++) {
-        for (int j = 0; j < ca; j++) {
-            if (tz[i][0] || tz[j][1]) {
-                a[i][j] = 0;
-            }
-        }
-    }
 
-    cout << "Adjusted matrix is:\n";
-    matprint(a, ra, ca);
-}
+  for (int i = 0; i < r; i++) { 
 
-int main() {
-    rczero();
-    return 0;
+        for (int j = 0; j < c; j++) { 
+
+            cout << a[i][j] << ' '; 
+
+        } 
+
+        cout << endl; 
+
+    } 
+
 }
